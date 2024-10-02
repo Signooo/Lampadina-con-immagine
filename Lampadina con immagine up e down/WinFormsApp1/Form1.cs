6729d216f49e7b20f0ab45a67a48b3178d32812e
Signooo <crisigno07@gmail.com>
2024-10-02 12:21:20 +0200

    Caricato file completo

---
+++ Lampadina con immagine up e down/WinFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int y = 167;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            y = y - 10;
+            pictureBox2.Location = new Point(321, y);
+            pictureBox1.Location = new Point(321, y);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            y = y + 10;
+            pictureBox2.Location = new Point(321, y);
+            pictureBox1.Location = new Point(321, y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+        }
+    }
+}
