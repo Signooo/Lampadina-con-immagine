647a6a01c108df6829b0929ca6f04adf771a7ade
SIGNORINI CRISTIAN <SIGNORINI.21287>
2024-10-02 12:37:02 +0200

    Inserito tramite prompt

--- Lampadina con immagine up e down/WinFormsApp1/Form1.cs
+++ Lampadina con immagine up e down/WinFormsApp1/Form1.cs
@@ -6,19 +6,19 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
-        int y = 167;
+        int y = 125;
         private void button3_Click(object sender, EventArgs e)
         {
             y = y - 10;
-            pictureBox2.Location = new Point(321, y);
-            pictureBox1.Location = new Point(321, y);
+            pictureBox2.Location = new Point(281, y);
+            pictureBox1.Location = new Point(281, y);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             y = y + 10;
-            pictureBox2.Location = new Point(321, y);
-            pictureBox1.Location = new Point(321, y);
+            pictureBox2.Location = new Point(281, y);
+            pictureBox1.Location = new Point(281, y);
         }
 
         private void button1_Click(object sender, EventArgs e)
